2b08a58c17e27a3f0a468a4c4fd4ba14c1281ce4
Morel121 <n.morel@alustudent.com>
2026-02-26 02:18:43 -0800

    nine

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-word = "Holberton"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print(f"First 3 letters:{word_first_3}")
-print(f"Last 2 letters:{word_last_2")
-print(f"Middle word: {middle_word}")
+str1 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str1 = str1[39:67] + str1[106:112] + str1[:6]
+print(str1)
