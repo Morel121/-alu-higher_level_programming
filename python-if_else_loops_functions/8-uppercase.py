41681aa4ed4a352181fd656dbef63137f9175493
Morel121 <n.morel@alustudent.com>
2026-02-26 03:50:21 -0800

    nine

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for charv in str:
+        if ord(char) >=c 97 and ord(char) <= 122:
+                                            char = chr(ord(char) - 32)
+                                        print("{}".format(char),
+end="")
+    print("")
