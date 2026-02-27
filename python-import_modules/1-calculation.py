d96438233244244b659fdebc92fb5acc2080ac61
Morel121 <n.morel@alustudent.com>
2026-02-27 03:08:32 -0800

    two

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 from calculator_1 import add, sub, mul, div
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
